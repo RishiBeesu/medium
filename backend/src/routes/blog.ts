fb6ed812b845f6e68bf63c285a45943ac2a8ec5e
RishiBeesu <rishibeesu17@gmail.com>
2024-10-24 23:27:29 +0530

    added middleware and blog routes logic

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,24 +1,92 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: {
+    userId: string;
+  };
 }>();
 
-blogRouter.post("/api/v1/blog", (c) => {
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  const [type, token] = authHeader.split(" ");
+  try {
+    const decoded = await verify(token, c.env.JWT_SECRET);
+    // Todo: fix below type error
+    // @ts-ignore
+    c.set("userId", decoded.userId);
+    await next();
+  } catch (e) {
+    c.status(403);
+    return c.json({
+      message: "Forbidden",
+    });
+  }
+});
+
+blogRouter.post("/api/v1/blog", async (c) => {
+  const userId = c.get("userId");
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const blog = await prisma.blog.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: userId,
+    },
+  });
+  return c.json({
+    id: blog.id,
+  });
+});
+
+blogRouter.put("/api/v1/blog", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const blog = await prisma.blog.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+  return c.json({
+    message: "Blog updated",
+  });
+});
+
+blogRouter.get("/api/v1/blog/:id", async (c) => {
+  const id = c.req.param("id");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const blog = await prisma.blog.findFirst({
+    where: {
+      id,
+    },
+  });
   return c.text("Hello Hono!");
 });
 
-blogRouter.put("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-
-blogRouter.get("/api/v1/blog/:id", (c) => {
-  return c.text("Hello Hono!");
-});
-
-blogRouter.get("/api/v1/blog/bulk", (c) => {
-  return c.text("Hello Hono!");
+// Todo: add pagination
+blogRouter.get("/api/v1/blog/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const blogs = await prisma.blog.findMany();
+  return c.json({
+    blogs,
+  });
 });
